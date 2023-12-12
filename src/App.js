3c3d1d6807718ab57a36e4e9c72dc5fe1cccd036
gabriela_p <palomog5@gmail.com>
2023-12-12 18:03:42 -0500

    Added search functionality

--- src/App.js
+++ src/App.js
@@ -29,6 +29,48 @@ class App extends Component {
     this.setState({ notes: [newNote, ...this.state.notes] });
   };
 
+  onType = (editMeId, updatedKey, updatedValue) => {
+    // editMeId == id of the note that is edited
+    // updatedKey == title or description field
+    // updatedValue == value of title or description
+    const updatedNotes = this.state.notes.map((note) => {
+      if (note.id !== editMeId) {
+        return note;
+      } else {
+        if (updatedKey === "title") {
+          note.title = updatedValue;
+          return note;
+        } else {
+          note.description = updatedValue;
+          return note;
+        }
+      }
+    });
+    this.setState({ notes: updatedNotes });
+  };
+
+  onSearch = (text) => {
+    const newSearchText = text.toLowerCase();
+    const updatedNotes = this.state.notes.map((note) => {
+      if (!newSearchText) {
+        note.doesMatchSearch = true;
+        return note;
+      } else {
+        const title = note.title.toLowerCase();
+        const description = note.description.toLowerCase();
+        const titleMatch = title.includes(newSearchText);
+        const descriptionMatch = description.includes(newSearchText);
+        const hasMatch = titleMatch || descriptionMatch;
+        note.doesMatchSearch = hasMatch;
+        return note;
+      }
+    });
+    this.setState({
+      notes: updatedNotes,
+      searchText: newSearchText
+    });
+  };
+
   render() {
     return (
       <div>
